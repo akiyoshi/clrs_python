cb9e5976f9947a786402fa22f02e6c673f23692b
akiyoshi <akiyoshi@users.noreply.github.com>
2018-03-06 21:48:54 +0900

    add 2.3-2

---
+++ ch2/2.3-2.py
@@ -0,0 +1,33 @@
+def merge(A, p, q, r):
+	n1 = q - p + 1
+	n2 = r - q
+
+	L = []
+	R = []
+
+	for i in range(0, n1):
+		L.append(A[p+i])
+
+	for j in range(0, n2):
+		R.append(A[q+j])
+
+	i = 1
+	j = 1
+
+	for k in range(p,r+1):
+		if i > L.length:
+			A[k:r] = R[j:]
+			break
+
+		if j > R.length:
+			A[k:r] = L[i:]
+			break
+
+
+		if L[i] <= R[j]:
+			A[k] = L[i]
+			i = i + 1
+		else:
+			A[k] = R[j]
+			j = j + 1
+
